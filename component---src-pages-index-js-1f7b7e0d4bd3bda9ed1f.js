1514a68d6fe70b05735fa6a1b6c77d14947b40fb
junray <junray@gmail.com>
2019-01-09 23:15:29 +0100

    Updates

--- component---src-pages-index-js-1f7b7e0d4bd3bda9ed1f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{139:function(e,a,n){"use strict";n.r(a);var t=n(0),l=n.n(t),o=n(143),c=n(145),i=n(144);a.default=function(){return l.a.createElement(c.a,null,l.a.createElement(i.a,{title:"Home",keywords:["gatsby","application","react"]}),l.a.createElement("h2",null,"La musica è un pallone"),l.a.createElement("p",null,"Il nuovo disco dei Klippa Kloppa"),l.a.createElement(o.Link,{to:"/page-2/"},"Acquistalo"))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-1f7b7e0d4bd3bda9ed1f.js.map
