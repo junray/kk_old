65b889fdf1616ec079ea79e87ee7f20e9d9ddf35
marco di gennaro <marco.digennaro@casevacanza.it>
2019-02-08 16:05:32 +0100

    Updates

--- component---src-pages-index-js-1df2adf9c1fd6d03db6d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{138:function(e,a,n){"use strict";n.r(a);var l=n(0),t=n.n(l),o=(n(142),n(144)),c=n(143);a.default=function(){return t.a.createElement(o.a,null,t.a.createElement(c.a,{title:"Home",keywords:["gatsby","application","react"]}),t.a.createElement("h2",null,"La musica è un pallone"),t.a.createElement("p",null,"Il nuovo disco dei Klippa Kloppa"),t.a.createElement("p",null,"Coming soon"))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-1df2adf9c1fd6d03db6d.js.map
---
+++ component---src-pages-index-js-7dec84191881de047bc1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{138:function(e,a,n){"use strict";n.r(a);var l=n(0),t=n.n(l),o=n(144),c=n(143);a.default=function(){return t.a.createElement(o.a,null,t.a.createElement(c.a,{title:"Home",keywords:["gatsby","application","react"]}),t.a.createElement("h2",null,"La musica è un pallone"),t.a.createElement("p",null,"Il nuovo disco dei Klippa Kloppa"),t.a.createElement("p",null,"Coming soon"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-7dec84191881de047bc1.js.map
